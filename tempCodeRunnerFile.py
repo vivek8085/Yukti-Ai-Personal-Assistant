69512414d74613dcdc5c32c3306bb99e6f86236b
Vivek Lokolakar <168566636+vivek8085@users.noreply.github.com>
2024-12-18 10:12:41 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import speech_recognition as sr
+from gtts import gTTS
+import os
+import tempfile
+import datetime
+import wikipedia
+import webbrowser
+import smtplib
+import time
+import base64
+import webbrowser
